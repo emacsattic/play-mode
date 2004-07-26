949901e5f3d53e7261b2e949de28df6515f112a3
mordred <>
2004-07-26 18:38:31 +0000

    Changed build-depends line.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from distutils.core import setup
 
 setup (name = "Plywood",
-       version = "0.5.1",
+       version = "0.5.10",
        description = "A system for typing and typesetting plays",
        author = "Monty Taylor",
        author_email = "mordred@inaugust.com",
